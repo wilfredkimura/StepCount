557ef50fdcf4fa922018adbd8df1d731dc223ec3
wilfredkimura <wilfredmutahi9@gmail.com>
2026-08-13 19:32:04 +0300

    chore(release): bump version to 1.1.0 (versionCode 2)

--- android/StepCount/app/build.gradle.kts
+++ android/StepCount/app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.example.stepcount"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
